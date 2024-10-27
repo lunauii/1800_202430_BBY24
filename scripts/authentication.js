fc47cd0dd7af18e587a28a6700a61caf24d184a8
Angelo Santos <angelo.rws@gmail.com>
2024-10-27 13:38:24 -0700

    Added base firebase components.

---
+++ scripts/authentication.js
@@ -0,0 +1,39 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: "main.html",
+    signInOptions: [
+    
+        /* 
+        *  FROM ANGELO
+        *  The Google and Facebook sign-in options are commented out because they require additional setup
+        *  and I am not sure if we want to use them. I have left them here for reference. Same goes for
+        *  the Phone sign-in option.
+        */
+    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  ui.start('#firebaseui-auth-container', uiConfig);
