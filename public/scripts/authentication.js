299253596f0a1fe3910da778d752b9d2540d5d9d
Angelo Santos <angelo.rws@gmail.com>
2024-10-30 13:00:52 -0700

    Fix login.html, add logo to login.html

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -46,8 +46,6 @@ var uiConfig = {
     signInSuccessUrl: 'html/main.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
