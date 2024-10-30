11af828b2c85c6f3e94966fbac28fcc75dd7e630
Angelo Santos <angelo.rws@gmail.com>
2024-10-30 14:00:03 -0700

    Add logout functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,6 +5,7 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
+        location.reload(); //reload the page
       }).catch((error) => {
         // An error happened.
       });
