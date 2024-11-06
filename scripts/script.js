689d954e648fb0be421fdb5a109436229889e017
Angelo Santos <angelo.rws@gmail.com>
2024-11-06 13:59:10 -0800

    Fix a lot of login related systems

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
+        window.location.replace("/login.html");
       }).catch((error) => {
         // An error happened.
         console.log("Error logging out.");
