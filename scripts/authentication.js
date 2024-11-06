ba0e20f9f4ac41b0433ca52f8ebdfac32b009ef1
Angelo Santos <angelo.rws@gmail.com>
2024-11-06 11:07:40 -0800

    Hot fix for login

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -26,7 +26,7 @@ var uiConfig = {
                    email: user.email,                         //with authenticated user's ID (user.uid)
             }).then(function () {
                    console.log("New user added to firestore");
-                   window.location.assign("/main");       //re-direct to main.html after signup
+                   window.location.assign("/home");       //re-direct to main.html after signup
             }).catch(function (error) {
                    console.log("Error adding new user: " + error);
             });
@@ -43,7 +43,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '/main',
+    signInSuccessUrl: '/home',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.EmailAuthProvider.PROVIDER_ID
