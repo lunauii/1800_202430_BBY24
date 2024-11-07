214c561813cfadca536b040a6102c94e5ba884c0
Angelo Santos <angelo.rws@gmail.com>
2024-11-06 17:25:39 -0800

    Implement reviews (no visual)

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -24,7 +24,7 @@ function displayHikeInfo() {
 }
 displayHikeInfo();
 
-function saveHikeDocumentIDAndRedirect(){
+function saveRestaurantDocumentIDAndRedirect(){
     let params = new URL(window.location.href) //get the url from the search bar
     let ID = params.searchParams.get("docID");
     localStorage.setItem('restaurantDocID', ID);
