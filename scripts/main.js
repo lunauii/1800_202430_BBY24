1ee00ad65cee680a3e754913adf140874d057cad
Angelo Santos <angelo.rws@gmail.com>
2024-11-06 16:47:54 -0800

    Greet the user in home/index.html

--- scripts/main.js
+++ scripts/main.js
@@ -15,19 +15,21 @@ function getNameFromAuth() {
   firebase.auth().onAuthStateChanged(user => {
       // Check if a user is signed in:
       if (user) {
-          // Do something for the currently logged-in user here: 
-          console.log(user.uid); //print the uid in the browser console
-          console.log(user.displayName);  //print the user name in the browser console
-          userName = user.displayName;
+          // Go to the correct user document by referencing to the user uid
+          currentUser = db.collection("users").doc(user.uid)
+            
+          // Get the document for current user
+          currentUser.get()
+              .then(userDoc => {
+                  // Get the data fields of the user
+                  let userName = userDoc.data().name;
+                  /* let userSchool = userDoc.data().school;
+                  let userCity = userDoc.data().city; */
 
-          //method #1:  insert with JS
-          //document.getElementById("name-goes-here").innerText = userName;    
-
-          //method #2:  insert using jquery
-          $("#name-goes-here").text(userName); //using jquery
-
-          //method #3:  insert using querySelector
-          //document.querySelector("#name-goes-here").innerText = userName
+                  // If the data fields are not empty, then write them in to the form.
+                  if (userName != null) {
+                      document.getElementById("name-goes-here").innerHTML = userName;
+                  }})
 
       } else {
           // No user is signed in.
