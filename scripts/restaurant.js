600ada1371204b32f401f38e4b803dd68c8d6df0
Angelo Santos <angelo.rws@gmail.com>
2024-11-07 12:52:24 -0800

    Redesign home/index.html

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -1,4 +1,4 @@
-function displayHikeInfo() {
+function displayRestaurantInfo() {
     let params = new URL( window.location.href ); //get URL of search bar
     let ID = params.searchParams.get( "docID" ); //get value for key "id"
     console.log( ID );
@@ -22,7 +22,7 @@ function displayHikeInfo() {
             imgEvent.src = "/images/" + restaurantCode + ".jpg"; */
         } );
 }
-displayHikeInfo();
+displayRestaurantInfo();
 
 function saveRestaurantDocumentIDAndRedirect(){
     let params = new URL(window.location.href) //get the url from the search bar
@@ -30,3 +30,57 @@ function saveRestaurantDocumentIDAndRedirect(){
     localStorage.setItem('restaurantDocID', ID);
     window.location.href = 'review.html';
 }
+
+function populateReviews() {
+    console.log("test");
+    let restaurantCardTemplate = document.getElementById("reviewCardTemplate");
+    let restaurantCardGroup = document.getElementById("reviewCardGroup");
+
+    let params = new URL(window.location.href); // Get the URL from the search bar
+    let restaurantID = params.searchParams.get("docID");
+
+    // Double-check: is your collection called "Reviews" or "reviews"?
+    db.collection("reviews")
+        .where("restaurantDocID", "==", restaurantID)
+        .get()
+        .then((allReviews) => {
+            reviews = allReviews.docs;
+            console.log(reviews);
+            reviews.forEach((doc) => {
+                var title = doc.data().title;
+                var description = doc.data().description;
+                var allergies = doc.data().allergies;
+                var time = doc.data().timestamp.toDate();
+                var rating = doc.data().rating; // Get the rating value
+                console.log(rating)
+
+                console.log(time);
+
+                let reviewCard = restaurantCardTemplate.content.cloneNode(true);
+                reviewCard.querySelector(".title").innerHTML = title;
+                reviewCard.querySelector(".time").innerHTML = new Date(
+                    time
+                ).toLocaleString();
+                reviewCard.querySelector(".allergies").innerHTML = `<b>Has my allergies:</b> ${allergies}`;
+                reviewCard.querySelector( ".description").innerHTML = `<b>Description:</b> ${description}`;
+
+                // Populate the star rating based on the rating value
+                
+	              // Initialize an empty string to store the star rating HTML
+								let starRating = "";
+								// This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+                for (let i = 0; i < rating; i++) {
+                    starRating += '<span class="material-icons">star</span>';
+                }
+								// After the first loop, this second loop runs from i=rating to i<5.
+                for (let i = rating; i < 5; i++) {
+                    starRating += '<span class="material-icons">star_outline</span>';
+                }
+                reviewCard.querySelector(".star-rating").innerHTML = starRating;
+
+                restaurantCardGroup.appendChild(reviewCard);
+            });
+        });
+}
+
+populateReviews();
