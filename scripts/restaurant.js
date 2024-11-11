1c3a5c34c1a45e956a22dcca994cd814f29f7a1e
JamesDASmith <James.DASmith649@gmail.com>
2024-11-10 18:15:51 -0800

    added template cards for menus, to be finished

--- scripts/restaurant.js
+++ scripts/restaurant.js
@@ -22,7 +22,7 @@ function displayHikeInfo() {
             imgEvent.src = "/images/" + restaurantCode + ".jpg"; */
         } );
 }
-displayHikeInfo();
+displayRestaurantInfo();
 
 function saveRestaurantDocumentIDAndRedirect(){
     let params = new URL(window.location.href) //get the url from the search bar
@@ -30,3 +30,36 @@ function saveRestaurantDocumentIDAndRedirect(){
     localStorage.setItem('restaurantDocID', ID);
     window.location.href = 'review.html';
 }
+
+
+
+// TO DO V
+
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("menuItemCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allmenu=> {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var item = doc.data().name;       // get value of the "name" key
+                var price = doc.data().price; //gets the length field
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-item').innerHTML = item;
+                newcard.querySelector('.card-price').innerHTML = price;
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("menu");  //input param is the name of the collection
